d93e9514f3ce691524bfa7df20f4a2258869b36f
Alexander <37336786+MrKerson@users.noreply.github.com>
2026-08-19 23:59:14 +0300

    Add experimental multi-gang switch toggle instances

--- custom_components/yandex_smart_home/schema/capability_toggle.py
+++ custom_components/yandex_smart_home/schema/capability_toggle.py
@@ -12,6 +12,10 @@ class ToggleCapabilityInstance(StrEnum):
     """Instance of a toggle capability.
 
     https://yandex.ru/dev/dialogs/smart-home/doc/concepts/toggle-instance.html
+
+    ``LEFT``/``RIGHT``/``CENTER`` and ``CHANNEL_*`` are experimental
+    instances used by this fork to test multi-gang wall switches in Yandex
+    Smart Home. They are not part of the public Yandex Smart Home API.
     """
 
     BACKLIGHT = "backlight"
@@ -21,6 +25,19 @@ class ToggleCapabilityInstance(StrEnum):
     MUTE = "mute"
     OSCILLATION = "oscillation"
     PAUSE = "pause"
+
+    # Experimental multi-gang switch instances.
+    LEFT = "left"
+    RIGHT = "right"
+    CENTER = "center"
+    CHANNEL_1 = "channel_1"
+    CHANNEL_2 = "channel_2"
+    CHANNEL_3 = "channel_3"
+    CHANNEL_4 = "channel_4"
+    CHANNEL_5 = "channel_5"
+    CHANNEL_6 = "channel_6"
+    CHANNEL_7 = "channel_7"
+    CHANNEL_8 = "channel_8"
 
 
 class ToggleCapabilityParameters(APIModel):
